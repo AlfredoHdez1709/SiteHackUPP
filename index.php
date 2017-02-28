a8ad8e73d6498d7156227f2c8ecd86a3ab7206f2
Alfredo Hernández <alfredo_hernandez_rguez@outlook.com>
2017-02-28 10:20:15 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <title>Hack-UPP</title>
+</head>
+<body>
+    <div id="wrapper">
+        <header id="header" class="alt">
+			<a href="index.php" class="logo"><strong>Hack-UPP</strong> <span>by MSTCUPP</span></a>
+			    <nav>
+				    <a href="#menu">Menu</a>
+			    </nav>
+		</header>
+
+        <nav id="menu">
+		    <ul class="links">
+                <li><a href="index.html">Home</a></li>
+				<li><a href="landing.html">Landing</a></li>
+				<li><a href="generic.html">Generic</a></li>
+    			<li><a href="elements.html">Elements</a></li>
+			</ul>
+			<ul class="actions vertical">
+				<li><a href="#" class="button special fit">Get Started</a></li>
+				<li><a href="#" class="button fit">Log In</a></li>
+			</ul>
+		</nav>
+
+        <section id="banner" class="major">
+			<div class="inner">
+				<header class="major">
+					<h1>Hack-UPP 2.0</h1>
+				</header>
+		    	<div class="content">
+			    	<p>Demuestra y comparte tus conocimientos por 24 horas consecutivas<br />
+							Hackea los problemas de hidalgo</p>
+			      <ul class="actions">
+			          <li><a href="#one" class="button next scrolly">Ver mas...</a></li>
+			      </ul>
+                  <ul class="actions">
+                      <li><a href="#two" class="button next scrolly">Registrate</a></li>
+                  </ul>
+			    </div>
+			</div>
+		</section>
+
+    
+    </div>
+</body>
+</html>
